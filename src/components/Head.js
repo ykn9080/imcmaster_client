c7b5d389446075efa7ef593c700c0ff18595bf6c
ykn9080 <ykn9080@empal.com>
2020-01-23 10:56:47 +0900

    global variable setting

--- src/components/Head.js
+++ src/components/Head.js
@@ -7,28 +7,23 @@ import MultiDispatch from "../reducers/multipleDispatch";
 
 const Head = () => {
   const counter = useSelector(state => state.counter);
-  const gvar1 = useSelector(state => state.global);
+  const global = useSelector(state => state.global);
   const dispatch = useDispatch();
-  const gval = { good: "low", happy: "girl" };
   MultiDispatch({ fast: "slow", ugly: "handsom" });
-  useEffect(() => {
-    // Update the document title using the browser API
-    // const typelist = Object.keys(gval);
-    // typelist.map(typ => {
-    //   dispatch(globalVariable({ [typ]: gval[typ] }));
-    // });
-  }, []);
-
   return (
     <>
       <h3>Counter: {counter}</h3>
       <Button onClick={() => dispatch(increment())}>+</Button>
       <Button onClick={() => dispatch(decrement())}>-</Button>
-      <Button onClick={() => dispatch(globalVariable(gval))}>init</Button>
+      <Button
+        onClick={() => dispatch(globalVariable({ good: "low", happy: "girl" }))}
+      >
+        init
+      </Button>
       <Button onClick={() => dispatch(globalVariable({ four: "b123ad" }))}>
         chg
       </Button>
-      <Button onClick={() => console.log(gvar1, gvar1.three)}>chg</Button>
+      <Button onClick={() => console.log(global, global.three)}>chg</Button>
     </>
   );
 };
--- src/reducers/multipleDispatch.js
+++ src/reducers/multipleDispatch.js
@@ -3,6 +3,10 @@ import { useDispatch } from "react-redux";
 import { globalVariable } from "../actions";
 
 const MultiDispatch = obj => {
+  /* 
+for setting global variable easily
+ex) MultiDispatch({name:"ykn", pass:"9080"})
+*/
   const dispatch = useDispatch();
   useEffect(() => {
     const typelist = Object.keys(obj);

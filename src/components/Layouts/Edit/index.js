931a6905c7468542d0267c4fde12e360d0349c46
ykn9080 <ykn9080@empal.com>
2020-02-15 17:10:36 +0900

    reset dom element failed...

--- src/components/Layouts/Edit/index.js
+++ src/components/Layouts/Edit/index.js
@@ -9,6 +9,7 @@ import { Sortable } from "./MenuSortable";
 import { makeStyles } from "@material-ui/core/styles";
 import { globalVariable } from "../../../actions";
 import Grid from "@material-ui/core/Grid";
+import { findChild } from "../../functions/findChildrens";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,15 +21,15 @@ const useStyles = makeStyles(theme => ({
     color: theme.palette.text.secondary
   }
 }));
-const findMenu = (menuData, comp, pid) => {
-  return menuData
+const findMenu = (tempMenu, comp, pid) => {
+  return tempMenu
     .filter((item, itemIndex) => item.comp === comp && item.pid === pid)
     .sort(function(a, b) {
       return a.seq < b.seq ? -1 : 1;
     });
 };
-const findControl = (menuData, comp, id) => {
-  const ctr = menuData.filter(
+const findControl = (tempMenu, comp, id) => {
+  const ctr = tempMenu.filter(
     (item, itemIndex) => item.comp === comp && item.id === id
   );
 
@@ -42,17 +43,25 @@ const Edit = props => {
   const [forchg, setForchg] = useState("");
   const dispatch = useDispatch();
   let menuData = useSelector(state => state.global.menu);
+  let selectedKey = useSelector(state => state.global.selectedKey);
   if (!menuData) menuData = JSON.parse(localStorage.getItem("menu"));
-  let topMenu, subMenu, control;
-  topMenu = findMenu(menuData, "1", "");
-  subMenu = findMenu(menuData, "1", topMenu[0].id);
-  useEffect(() => {
-    dispatch(globalVariable({ subMenu: subMenu }));
-    //$(".dropli:first-child").click();
-  }, []);
+  let subMenu, tempMenu;
+  dispatch(globalVariable({ tempMenu: menuData }));
+  //$(".dropli:first-child").click();
+  tempMenu = menuData;
 
-  subMenu = useSelector(state => state.global.subMenu);
-  const ctr = useSelector(state => state.global.control);
+  useEffect(() => {
+    //dispatch(globalVariable({ subMenu: subMenu }));
+    dispatch(globalVariable({ tempMenu: menuData }));
+    //$(".dropli:first-child").click();
+    tempMenu = menuData;
+  });
+  let topMenu, control;
+  topMenu = findMenu(tempMenu, "1", "");
+  subMenu = findMenu(tempMenu, "1", topMenu[0].id);
+
+  //subMenu = useSelector(state => state.global.subMenu);
+  //const ctr = useSelector(state => state.global.control);
 
   // let topMenu = findMenu("1", "");
   // console.log(topMenu);
@@ -60,17 +69,17 @@ const Edit = props => {
   // console.log(subMenu);
 
   const selectedmenu = id => {
-    const sub = findMenu(menuData, "1", id);
-
+    dispatch(globalVariable({ selectedKey: id }));
+    selectedKey = id;
+    const sub = findMenu(tempMenu, "1", id);
     if (sub.length == 0) {
-      const ctr = findControl(menuData, "1", id);
+      const ctr = findControl(tempMenu, "1", id);
       dispatch(globalVariable({ control: ctr }));
       return false;
     }
-    dispatch(globalVariable({ selectedKey: id }));
-    dispatch(globalVariable({ subMenu: sub }));
     markTab(id);
     //console.log(id, findMenu("1", id));
+    setForchg("");
   };
 
   const markTab = id => {
@@ -90,7 +99,7 @@ const Edit = props => {
     });
     addControl(ctrList);
   };
-
+  console.log("reload");
   // const submenu = findMenu("1", topmenu[0].id);
   const classes = useStyles();
 
@@ -99,18 +108,18 @@ const Edit = props => {
       <div className={classes.root}>
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <HeadEdit topdata={topMenu} selectedmenu={selectedmenu} />
+            <HeadEdit selectedmenu={selectedmenu} />
           </Grid>
           <Grid item xs={3}>
             <Sortable
               opacity={0.1}
-              data={menuData}
-              // topdata={subMenu}
+              pid={selectedKey}
+              depth={"all"}
               liclass={"dropli"}
               selectedmenu={selectedmenu}
             />
             <Button>Add</Button>
-            {/*<SubMenu topdata={submenu} data={menuData} pid={topmenu[0].id} /> */}
+            {/*<SubMenu topdata={submenu} data={tempMenu} pid={topmenu[0].id} /> */}
           </Grid>
           <Grid item xs={9}>
             <Body addControl={addControl} removeControl={removeControl} />
---
+++ src/components/functions/findChildrens.js
@@ -0,0 +1,68 @@
+import _ from "lodash";
+
+//하위의 모든 자식을 리턴하는 프로그램
+//Call find children function for 1004
+// findChildrens(arr, "1004", [], res => {
+//   console.log("result for 1004", res);
+// });
+
+// const arr = [
+//   { id: "1001", depth: "1", pid: "0" },
+//   { id: "1002", depth: "1", pid: "0" },
+//   { id: "1003", depth: "2", pid: "1001" },
+//   { id: "1004", depth: "2", pid: "1001" },
+//   { id: "1005", depth: "3", pid: "1003" },
+//   { id: "1006", depth: "3", pid: "1004" },
+//   { id: "1007", depth: "4", pid: "1006" },
+//   { id: "1008", depth: "4", pid: "1006" },
+//   { id: "1009", depth: "5", pid: "1008" }
+// ];
+
+export default function findChildrens(data, id, oputArr, callback) {
+  let filterArr = data.filter(({ pid }) => pid == id);
+  if (filterArr.length) {
+    //Concat array with filtered data
+    oputArr = [...oputArr, ...filterArr.map(({ id }) => id)];
+    //Recursive call for again search next node data
+    findChildrens(data, oputArr[oputArr.length - 1], oputArr, callback);
+  } else {
+    //If find
+    callback(oputArr);
+  }
+}
+//pid를 parent로 갖는 objects만 추출
+export const directChild = (data, pid, seq) => {
+  console.log(data, pid, seq);
+  return data
+    .filter((subitem, itemIndex) => subitem.pid === pid)
+    .sort(function(a, b) {
+      return a[seq] < b[seq] ? -1 : 1;
+    });
+};
+
+let res = [];
+//not callback childFind(data,"id");
+export const findChild = (data, id, sortFields, sortOrder) => {
+  data.forEach(obj => {
+    if (obj.pid === id) {
+      res.push(obj);
+      findChild(data, obj.id);
+    }
+  });
+  if (typeof sortFields !== "undefined")
+    res = sortBy(res, sortFields, sortOrder);
+  return res;
+};
+
+//fields:['a','b'], orderby:['asc','asc']
+// var data = [{
+//   a: 'a',
+//   b: 2
+// }, {
+//   a: 'a',
+//   b: 1
+// }]
+export const sortBy = (data, fields, orderby) => {
+  if (typeof orderby === "undefiend") orderby = [];
+  return _.sortBy(data, fields, orderby);
+};

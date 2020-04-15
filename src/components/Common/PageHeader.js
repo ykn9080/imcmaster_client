e4f4da1b3af8e1851ec603900dc93936a75b143d
ykn9080 <ykn9080@empal.com>
2020-04-15 18:43:50 +0900

    PageHeader Summary as children component

--- src/components/Common/PageHeader.js
+++ src/components/Common/PageHeader.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useLocation, useHistory, Link } from "react-router-dom";
 import $ from "jquery";
-import { PageHeader, Breadcrumb } from "antd";
+import { PageHeader, Breadcrumb, Row, Col, Statistic } from "antd";
 import AntBreadCrumb from "./BreadCrumb";
 
 const PageHead = (props) => {
@@ -36,15 +36,51 @@ const PageHead = (props) => {
     title: props.title,
   };
   if (props.onBack) pageProps = { ...pageProps, onBack: onBack };
-  if (typeof props.subTitle != "undefined")
-    pageProps = { ...pageProps, subTitle: props.subtitle };
+  if (props.subTitle) pageProps = { ...pageProps, subTitle: props.subtitle };
+  if (props.extra) pageProps = { ...pageProps, extra: props.extra };
+
+  /* #region  children component example*/
+  const Description = ({ term, children, span = 12 }) => (
+    <Col span={span}>
+      <div className="description">
+        <div className="term">{term}</div>
+        <div className="detail">{children}</div>
+      </div>
+    </Col>
+  );
+  const content = (
+    <Row>
+      <Description term="Created">Lili Qu</Description>
+      <Description term="Association">
+        <a>421421</a>
+      </Description>
+      <Description term="Creation Time">2017-01-10</Description>
+      <Description term="Effective Time">2017-10-10</Description>
+      <Description term="Remarks" span={24}>
+        Gonghu Road, Xihu District, Hangzhou, Zhejiang, China
+      </Description>
+    </Row>
+  );
+  const extraContent = (
+    <img
+      src="https://gw.alipayobjects.com/mdn/mpaas_user/afts/img/A*KsfVQbuLRlYAAAAAAAAAAABjAQAAAQ/original"
+      alt="content"
+    />
+  );
+  const child = (
+    <div className="wrap">
+      <div className="content padding">{content}</div>
+      <div className="extraContent">{extraContent}</div>
+    </div>
+  );
+  /* #endregion */
 
   return (
     <>
       <div style={{ paddingLeft: 25, paddingTop: 5 }}>
         <AntBreadCrumb />
       </div>
-      <PageHeader {...pageProps} />
+      <PageHeader {...pageProps}>{props.children}</PageHeader>
     </>
   );
 };

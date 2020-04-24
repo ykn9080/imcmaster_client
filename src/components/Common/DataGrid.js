3cdcc797404773501ddd8084355082a81e4d7b05
ykn9080 <ykn9080@empal.com>
2020-04-24 15:43:34 +0900

    data grid test

---
+++ src/components/Common/DataGrid.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDataGrid from "react-data-grid";
+
+const DataGrid = (props) => {
+  let rowCount = 3,
+    minHeight = 150;
+  let columns = [
+    { key: "id", name: "ID" },
+    { key: "title", name: "Title" },
+    { key: "count", name: "Count" },
+  ];
+
+  let rows = [
+    { id: 0, title: "row1", count: 20 },
+    { id: 1, title: "row1", count: 40 },
+    { id: 2, title: "row1", count: 60 },
+  ];
+  //   if (props.rowCount) rowCount = props.rowCount;
+  //   if (props.minHeight) minHeight = props.minHeight;
+  //   if (props.columns) columns = props.columns;
+  //   if (props.rows) rows = props.rows;
+
+  return (
+    <ReactDataGrid
+      columns={columns}
+      rowGetter={(i) => rows[i]}
+      rowsCount={rowCount}
+      minHeight={minHeight}
+    />
+  );
+};
+
+export default DataGrid;
---
+++ src/components/Common/DataGrid1.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import ReactDataGrid from "react-data-grid";
+
+const formatter = ({ value }) => {
+  return <span class="label label-info">{value}</span>;
+};
+
+function createColumns(count) {
+  return [...Array(count).keys()].map((i) => ({
+    key: `col${i}`,
+    name: `Column ${i}`,
+    width: 160,
+    editable: true,
+    formatter,
+  }));
+}
+
+function createSingleRow(columns, rowIdx) {
+  return columns.reduce(
+    (row, c, cidx) => ({ ...row, [c.key]: `Row ${rowIdx} - Col ${cidx}` }),
+    {}
+  );
+}
+
+function createRows(count, columns) {
+  return [...Array(count).keys()].map((rowIdx) =>
+    createSingleRow(columns, rowIdx)
+  );
+}
+
+const columns = createColumns(50);
+
+class DataGrid1 extends React.Component {
+  state = { rows: createRows(1000, columns) };
+
+  onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
+    this.setState((state) => {
+      const rows = state.rows.slice();
+      for (let i = fromRow; i <= toRow; i++) {
+        rows[i] = { ...rows[i], ...updated };
+      }
+      return { rows };
+    });
+  };
+  render() {
+    return (
+      <div>
+        <ReactDataGrid
+          columns={columns}
+          rowGetter={(i) => this.state.rows[i]}
+          rowsCount={this.state.rows.length}
+          onGridRowsUpdated={this.onGridRowsUpdated}
+          enableCellSelect={true}
+          enableCellAutoFocus={false}
+          minHeight={650}
+        />
+      </div>
+    );
+  }
+}
+
+export default DataGrid1;
